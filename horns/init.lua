b4c88a8ca7bb4ddb48e216b20aa1e1bc817db783
notYoosung <notYoosung4901@gmail.com>
2024-08-23 18:34:54 -0500

    horns

--- horns/init.lua
+++ horns/init.lua
@@ -6,7 +6,7 @@ local function play_call(name, pos)
 	return minetest.sound_play(name, {
 		max_hear_distance = 256,
 		pos = pos,
-		
+
 	})
 end
 
@@ -25,10 +25,12 @@ end
 local custom_calls = scandir("./sounds/custom")
 local mc_calls = scandir("./sounds/mc")--]]
 
+local rawitemstring = [[
+horns/sounds/custom/horns_custom_amogus.ogg horns/sounds/custom/horns_custom_android_notification_earrape.ogg horns/sounds/custom/horns_custom_augh.ogg horns/sounds/custom/horns_custom_bonk.ogg horns/sounds/custom/horns_custom_bruh.ogg horns/sounds/custom/horns_custom_cave_noise.ogg horns/sounds/custom/horns_custom_discord_leave.ogg horns/sounds/custom/horns_custom_discord_ping.ogg horns/sounds/custom/horns_custom_erm_what_the_sigma.ogg horns/sounds/custom/horns_custom_fart.ogg horns/sounds/custom/horns_custom_fbi_open_up.ogg horns/sounds/custom/horns_custom_huh.ogg horns/sounds/custom/horns_custom_lego_yoda_death.ogg horns/sounds/custom/horns_custom_meme.ogg horns/sounds/custom/horns_custom_metal_pipe.ogg horns/sounds/custom/horns_custom_rick_roll.ogg horns/sounds/custom/horns_custom_shut_up.ogg horns/sounds/custom/horns_custom_taco_bell.ogg horns/sounds/custom/horns_custom_the_animatronic_characters_here_do_get_a_bit_quirky_at_night.ogg horns/sounds/custom/horns_custom_two_hours_later.ogg horns/sounds/custom/horns_custom_womp_womp_womp_womp.ogg horns/sounds/custom/horns_custom_yippee.ogg
+]]
+local custom_call_names = string.split(string.gsub(string.gsub(rawitemstring, "horns/sounds/custom/", ""), ".ogg", ""),
+" ")
 
-local custom_call_names = {
-	
-}
 
 local mc_call_names = {
 	"Ponder",
@@ -60,15 +62,17 @@ local function capitalize(str)
 	return str:sub(1, 1):upper() .. str:sub(2, str:len())
 end
 
+
 for i, v in ipairs(custom_call_names) do
 	local call_dir = custom_call_names[i] or "unnamed"
-	local call_name = call_dir:gsub("horn_custom_", "")
+	call_dir = call_dir:trim()
+	local call_name = call_dir:gsub("horns_custom_", "")
 
 	local on_use = function(itemstack, user, pointed_thing)
 		play_call(call_name, user:get_pos())
 	end
 	minetest.register_tool(modname .. ":horn_custom_" .. call_name, {
-		description = capitalize(call_name),
+		description = call_name:gsub("_", " "),
 		image = "goat_horn.png",
 		use_texture_alpha = "clip",
 		on_secondary_use = on_use,
